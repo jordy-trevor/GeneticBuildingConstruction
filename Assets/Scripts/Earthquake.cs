97af58795325b61a6bc5a5282f6b3908f21f2486
Trevor Jordy <tjordy@ucsc.edu>
2018-12-11 02:15:47 -0800

    earthquake engine more or less complete

---
+++ Assets/Scripts/Earthquake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earthquake : MonoBehaviour {
+
+    [Range(0.0f, 10.0f)][SerializeField] private float shakeDistance = 1.0f;
+    [Range(0.0f, 10.0f)] [SerializeField] private float speed = 3.0f;
+    private Rigidbody rb;
+    string moveDirection = "right";
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (this.transform.position.z > shakeDistance)
+        {
+            moveDirection = "left";
+        }
+        else if (this.transform.position.z < -1 * shakeDistance)
+        {
+            moveDirection = "right";
+        }
+
+        if (moveDirection == "right")
+        {
+            rb.MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
+        }
+        else if (moveDirection == "left")
+        {
+            rb.MovePosition(transform.position - transform.forward * Time.deltaTime * speed);
+        }
+       
+    }
+}
